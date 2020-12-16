0c670627c6fde6116d232a11466b405ef3eff6d4
Jonathan Ramus <44757120+Jonathan-Ramus@users.noreply.github.com>
2020-12-16 02:01:41 +0100

    Version 0.1

---
+++ README.md
@@ -0,0 +1,12 @@
+# Paywall-Checker-
+Paywall Checker is a google chrome extension that checks whether a link leads to a paywalled site. It compares the url to a master list found at https://raw.githubusercontent.com/Jonathan-Ramus/PaywallChecker/main/BList.txt. 
+
+# Motivation
+As far as I know, no plugin currently exists that allows users to identify paywalled news sites before accessing them. It is very annoying to click a link to a news article, only to find out you have to pay to access it. This plugin will highlight links to news sites that are paywalled.
+
+# Installation 
+Currently, the plugin can be installed by loading the files on chrome://extensions. You will need to enable developer mode to do this.
+
+# License 
+MIT
+
---
+++ content.js
@@ -0,0 +1,99 @@
+links = document.getElementsByTagName('a');
+
+for (i = 0;  i < links.length; i++) { //for every link, create a mouseover event 
+    links[i].onmouseover = function () { 
+        //saves default style to oldStyle attribute
+        var link = this;
+		if (this.hasAttribute("oldStyle") == false){ //checks whether oldstyle attribute exists 
+            var old = this.getAttribute("style"); 
+            this.setAttribute("oldStyle", old);
+        }
+        if (this.href != ""){
+		
+		var check = this.href + "";
+		var xhttp = new XMLHttpRequest(), searchString = "https://cors-anywhere.herokuapp.com/" + this.href;
+
+		xhttp.onreadystatechange = function() {
+
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+				var mark = xhttp.responseText.split('\n');
+				var i = 0
+				for (; i < mark.length - 1; i++){
+					if (check.indexOf(mark[i]) > -1){
+						console.log(check);
+						console.log(mark[i]);
+						invalidLink(link);
+						break;
+					}
+				}
+				if (i == mark.length - 1){
+					validLink(link);
+				}
+			}
+		};
+
+		xhttp.open("GET", "https://raw.githubusercontent.com/Jonathan-Ramus/PaywallChecker/main/BList.txt", true);
+		xhttp.send();
+		
+		}
+    }
+}
+
+function validLink(thisLink) { //changes doc to show it is valid 
+    chrome.storage.sync.get( //get greenBox and greenBoxTime values from popup saved to chrome.storage
+        ['greenBox', 'greenBoxTime'], function(result) { 
+        //console.log(result);
+        if (result.greenBox == true){
+            thisLink.style['background-color'] = "lightgreen";
+            thisLink.style.color = "black";
+            if (result.greenBoxTime == '2'){
+                setTimeout(function() {
+                    thisLink.style = thisLink.getAttribute("oldStyle"); 
+                }, 2000);            
+            }
+            else if (result.greenBoxTime == '5'){
+                setTimeout(function() {
+                    thisLink.style = thisLink.getAttribute("oldStyle"); 
+                }, 5000);  
+            }
+        }
+    });
+}
+
+function invalidLink(thisLink){ //changes doc to show link is invalid
+    chrome.storage.sync.get( //get unclickable and strikethrough values from popup saved to chrome.storage
+        ['unclickable', 'redBox', 'redBoxTime'], function(result) {
+        if (result.unclickable == true){
+            thisLink.removeAttribute("href"); //remove url
+        }
+        /*if (result.strikethrough == true){
+            thisLink.style.setProperty("text-decoration", "line-through"); //strikethrough link
+        }*/
+		if (result.redBox == true){
+            thisLink.style['background-color'] = "red";
+            thisLink.style.color = "black";
+            if (result.redBoxTime == '2'){
+                setTimeout(function() {
+                    thisLink.style = thisLink.getAttribute("oldStyle"); 
+                }, 2000);            
+            }
+            else if (result.redBoxTime == '5'){
+                setTimeout(function() {
+                    thisLink.style = thisLink.getAttribute("oldStyle"); 
+                }, 5000);  
+            }
+        }
+    });         
+}
+
+
+
+
+    
+
+
+
+
+
+
